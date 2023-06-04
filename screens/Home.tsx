8b304131b02dae3022ac9afc8749865e4c951bca
kamran <kamrankhan47@mail.ru>
2023-06-04 13:37:31 +0400

    salam

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,34 +1,24 @@
-import {
-  Alert,
-  FlatList,
-  Image,
-  Modal,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteBlog, getAllBlogs } from '../store/BlogSlice';
-import { AppDispatch, RootState } from '../store/Store';
+import { Alert, FlatList, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { deleteBlog, getAllBlogs } from '../store/BlogSlice';
 import { addFavorite } from '../store/FavoritesSlice';
+import { AppDispatch, RootState } from '../store/Store';
 
 const Home = ({ navigation }: any) => {
   const dispatch = useDispatch<AppDispatch>();
   const { blogs } = useSelector<RootState, any>((state) => state);
   const [searchText, setSearchText] = useState('');
-  const {FavoritesSlice} = useSelector<RootState, any>((state) => state);
-
-  const filteredBlogs = blogs.data.filter((blog: any) =>
-    blog.title.toLowerCase().includes(searchText.toLowerCase())
-  );
+  const [filteredData, setFilteredData] = useState(blogs.data);
 
   useEffect(() => {
     dispatch(getAllBlogs());
   }, []);
+
+  useEffect(() => {
+    setFilteredData(blogs.data);
+  }, [blogs.data]);
 
   useFocusEffect(
     React.useCallback(() => {
@@ -43,11 +33,18 @@ const Home = ({ navigation }: any) => {
   const deletemyBlog = (item: any) => {
     dispatch(deleteBlog(item));
   };
-  
+
   const addtoFav = (item: any) => {
-    dispatch(addFavorite(item))
-  }
-  
+    dispatch(addFavorite(item));
+  };
+
+  const searchFilter = (text: string) => {
+    const newData = blogs.data.filter((item: any) =>
+      item.title.toLowerCase().includes(text.toLowerCase())
+    );
+    setFilteredData(newData);
+    setSearchText(text);
+  };
 
   return (
     <View>
@@ -56,12 +53,12 @@ const Home = ({ navigation }: any) => {
           placeholder="Search"
           style={{ borderWidth: 2, marginHorizontal: 20 }}
           value={searchText}
-          onChangeText={(text) => setSearchText(text)}
+          onChangeText={searchFilter}
         />
       </View>
       <View style={{ borderWidth: 2, marginHorizontal: 10, marginTop: 10, height: 400 }}>
         <FlatList
-          data={filteredBlogs}
+          data={filteredData}
           renderItem={({ item }) => {
             return (
               <TouchableOpacity onPress={() => gotoDetail(item.id)}>
@@ -80,7 +77,7 @@ const Home = ({ navigation }: any) => {
                   </TouchableOpacity>
                   <TouchableOpacity onPress={()=>addtoFav(item)}>
                     <View style={{ marginLeft: 10, backgroundColor: 'purple' }}>
-                      <Text style={{ fontSize: 20, color: 'white' }}>addtoFav</Text>
+                      <Text style={{ fontSize: 20, color: 'white' }}>Add to Fav</Text>
                     </View>
                   </TouchableOpacity>
                 </View>
